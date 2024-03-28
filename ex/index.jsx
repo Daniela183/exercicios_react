24c8ac2af77c66fe7327c96288d5bd89d33d1163
Daniela Soares <77057567+Daniela183@users.noreply.github.com>
2024-03-27 22:16:12 -0300

    index.jsx

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import Field from './field'
-
-ReactDOM.render(
-    <Field initialValue='Teste' />
-, document.getElementById('app'))
+//teste

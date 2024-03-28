7a802bb4d4ab967e8aed9422b595b726162faa46
Daniela Soares <77057567+Daniela183@users.noreply.github.com>
2024-03-27 22:19:59 -0300

    reactUtils.js

--- utils/reactUtils.js
+++ utils/reactUtils.js
@@ -1,7 +1,7 @@
 //aula 406
 import React from 'react'
 
-// Adicionado no ex.14
+//add no ex.11
 function childrenWithProps(children, props) {
     return React.Children.map(children,
             child => React.cloneElement(child, {...props}))
